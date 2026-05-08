f7450318c9cd9d1db7729094323d80cd1a41429b
Sebas825 <157090360+Sebas825@users.noreply.github.com>
2026-05-08 10:56:47 -0500

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -19,7 +19,7 @@ class Servicio(ABC):
         self.precio = precio
 
     @abstractmethod
-    def calcular_costo(self, horas):
+    def calcular_costo(self, horas, descuento=0):
         pass
 
 
@@ -29,23 +29,22 @@ class Servicio(ABC):
 
 class Sala(Servicio):
 
-    def calcular_costo(self, horas):
+    def calcular_costo(self, horas, descuento=0):
 
-        return self.precio * horas
+        return (self.precio * horas) - descuento
 
 
 class Equipo(Servicio):
 
-    def calcular_costo(self, horas):
+    def calcular_costo(self, horas, descuento=0):
 
-        return (self.precio * horas) + 10
-
+        return (self.precio * horas + 10) - descuento
 
 class Asesoria(Servicio):
 
-    def calcular_costo(self, horas):
+    def calcular_costo(self, horas, descuento=0):
 
-        return (self.precio * horas) * 1.2
+        return (self.precio * horas * 1.2) - descuento
 
 
 # ============================================

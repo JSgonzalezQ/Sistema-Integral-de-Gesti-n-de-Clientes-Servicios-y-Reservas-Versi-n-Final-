b7b1658fae47a28d623e647698b19891360eb38f
herrerabreiner858-lgtm <herrerabreiner858@gmail.com>
2026-05-11 12:18:31 -0500

    Update reserva.py

--- reserva.py
+++ reserva.py
@@ -3,15 +3,17 @@ from excepciones import ErrorValidacion
 # ================================
 # CLASE RESERVA
 # ================================
+# Clase encargada de gestionar reservas
+
 class Reserva:
     def __init__(self, cliente, servicio, horas):
         self.cliente = cliente
         self.servicio = servicio
         self.horas = horas
-
+# Verifica que las horas sean válidas
     def procesar(self):
         if self.horas <= 0:
             raise ErrorValidacion("Horas inválidas")
-
+# Procesa la reserva y calcula el costo
         return self.servicio.calcular_costo(self.horas)
     
